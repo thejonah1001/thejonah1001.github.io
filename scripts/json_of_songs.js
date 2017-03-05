05f9785ede799507515d15056741dee75110dd3e
thejonah1001 <jonahalter1001@gmail.com>
2017-03-05 14:49:03 -0500

    initial json database of songs

---
+++ scripts/json_of_songs.js
@@ -0,0 +1,36 @@
+/* JSON of JONATHAN'S ALBUMS */
+var albums = [
+    {
+        title: "The Human Condition",
+        artist: "Jon Bellion",
+        cover_url: "https://upload.wikimedia.org/wikipedia/en/4/4d/The_Human_Condition_%28Official_Album_Cover%29_by_Jon_Bellion.png",
+        tracks: [
+            "He Is the Same",
+            "80's Films",
+            "All Time Low",
+            "New York Soul (Part II)",
+            "Fashion",
+            "Maybe IDK",
+            "Woke the Fuck Up",
+            "Overwhelming",
+            "Weight of the World",
+            "The Good in Me",
+            "Morning in America",
+            "iRobot",
+            "Guillotine",
+            "Hand of God"
+        ]
+    },
+    {
+        title: "",
+        artist: "",
+        cover_url: "",
+        tracks: [
+            "",
+            "",
+            /*...*/
+            "",
+            ""
+        ]
+    }
+];
